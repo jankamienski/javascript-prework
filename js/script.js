3bb018baf416c4b4cda28ba3d575c131f731511f
jankamienski <kamienski.jan@gmail.com>
2020-10-06 13:21:15 +0200

    Add variables and messages to script.js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let playerMove= 'papier';
+let computerMove= 'kamień';
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!')
+
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to kamień, to przegrywasz!');
