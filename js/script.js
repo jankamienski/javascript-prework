24b8125786a4e3ff2e384e202971a775e29e5f32
jankamienski <kamienski.jan@gmail.com>
2020-10-09 14:16:09 +0200

    Add changes 9.10.2020

--- js/script.js
+++ js/script.js
@@ -11,33 +11,28 @@ function getMoveName(randomNumber) {
     }
   }
 
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('Wylosowana liczba to: ' + randomNumber);
 
 let argComputerMove = getMoveName(randomNumber);
 printMessage('Mój ruch to: ' + argComputerMove);
 
-/*if (randomNumber == 1) {
-    computerMove = 'kamień';
-} else if (randomNumber == 2) {
-    computerMove = 'papier';
-} else if (randomNumber == 3) {
-    computerMove = 'nożyce';
-}*/
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+let argPlayerMove = playerInput;  //nie wiem czy to dobra dekalracja...
 console.log('Gracz wpisał: ' + playerInput);
-let argPlayerMove = getMoveName(randomNumber);
-
-/*if (playerInput == '1') {
-    playerMove = 'kamień';
+  
+  if (playerInput == '1') {
+    argPlayerMove = 'kamień';
 } else if (playerInput == "2") {
-    playerMove = 'papier';
+    argPlayerMove = 'papier';
 } else if (playerInput == "3") {
-    playerMove = 'nożyce';
-}*/
-
-printMessage('Twój ruch to: ' + ArgPlayerMove);
+    argPlayerMove = 'nożyce';
+} else {
+    printMessage('Błędna cyfra! Podaj 1, 2 lub 3');
+}
+printMessage('Twój ruch to: ' + argPlayerMove);
 
 
 function displayResult(argComputerMove, argPlayerMove){
